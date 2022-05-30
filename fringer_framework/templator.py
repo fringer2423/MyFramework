059d4d60909925ac080fec0b166ae30fd6848d89
Vladislav Aganin <vlad_aganin33@hotmail.com>
2022-05-30 18:55:48 +0300

    Реализовано наследование шаблонов и подключение подшаблонов

--- fringer_framework/templator.py
+++ fringer_framework/templator.py
@@ -1,18 +1,17 @@
-from os.path import join
-from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
     """
+    Минимальный пример работы с шаблонизатором
+    :param folder: папка с шаблонами
     :param template_name: имя шаблона
-    :param folder: папка в которой ищем шаблон
-    :param kwargs: параметры, передаваемые в шаблон
+    :param kwargs: параметры для передачи в шаблон
     :return:
     """
-    file_path = join(folder, template_name)
-    # Открываем шаблон по имени
-    with open(file_path, encoding='utf-8') as f:
-        # Читаем
-        template = Template(f.read())
-    # Рендерим шаблон с параметрами
+
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
--- views.py
+++ views.py
@@ -9,4 +9,4 @@ class Index:
 
 class About:
     def __call__(self, request):
-        return '200 OK', 'about'
+        return '200 OK', render('about.html')
